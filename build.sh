254f138a6ec32a3afca6a4b41cabd7c0c5c4aab5
390405712 <390405712@qq.com>
2019-08-26 15:51:11 +0800

    create blog

--- build.sh
+++ build.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env sh
 
+# 确保脚本抛出遇到的错误
 set -e
-npm run build
 
-cd dist
+# 生成静态文件
+npm run docs:build
+
+# 进入生成的文件夹
+cd docs/.vuepress/dist
 
 git init
 git add -A
 git commit -m 'deploy'
 
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:390405712/blog.git master:gh-pages
 
 cd -
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,32 +1,27 @@
 module.exports = {
-    base: '/vue-analysis/',
-    dest: 'dist',
-    title: 'Vue.js 技术揭秘',
-    description: 'Analysis vue.js deeply',
+    base: '/blog/',
+    // dest: 'dist',
+    title: "ExceedPeakWu's Blog",
+    description: 'study notes',
     head: [
         ['link', { rel: 'icon', href: `/logo.png` }],
-        ['link', {rel: 'manifest', href: '/manifest.json'}],
-        ['meta', {name: 'theme-color', content: '#3eaf7c'}],
-        ['meta', {name: 'apple-mobile-web-app-capable', content: 'yes'}],
-        ['meta', {name: 'apple-mobile-web-app-status-bar-style', content: 'black'}],
-        ['link', { rel: 'apple-touch-icon', href: `/icons/apple-touch-icon-152x152.png` }],
-        ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
-        ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
-        ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
+        // ['link', {rel: 'manifest', href: '/manifest.json'}],
+        // ['meta', {name: 'theme-color', content: '#3eaf7c'}],
+        // ['meta', {name: 'apple-mobile-web-app-capable', content: 'yes'}],
+        // ['meta', {name: 'apple-mobile-web-app-status-bar-style', content: 'black'}],
+        // ['link', { rel: 'apple-touch-icon', href: `/icons/apple-touch-icon-152x152.png` }],
+        // ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+        // ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
+        // ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
     ],
-    serviceWorker: false,
+    // serviceWorker: false,
     themeConfig: {
-        repo: 'ustbhuangyi/vue-analysis',
+        repo: 'https://github.com/390405712/blog',
         editLinks: true,
         docsDir: 'docs',
         editLinkText: '在 GitHub 上编辑此页',
         lastUpdated: '上次更新',
-        nav: [
-            {
-                text: '配套视频',
-                link: 'https://coding.imooc.com/class/228.html'
-            }
-        ],
+        nav: [],
         sidebar: [
             {
                 title: '准备工作',
