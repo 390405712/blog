92450acc3f6a5f01f9c1c84058d68cb35d2b3abe
390405712 <390405712@qq.com>
2019-08-26 14:24:44 +0800

    create blog

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    base: '/blog',
+    base: '/',
     dest: 'dist',
     title: "ExceedPeakWu's Blog",
     description: 'study notes',
@@ -9,16 +9,16 @@ module.exports = {
         // ['meta', {name: 'theme-color', content: '#be313f'}],
         // ['meta', {name: 'apple-mobile-web-app-capable', content: 'yes'}],
         // ['meta', {name: 'apple-mobile-web-app-status-bar-style', content: 'black'}],
-        ['link', { rel: 'apple-touch-icon', href: `/icons/apple-touch-icon-152x152.png` }],
+        // ['link', { rel: 'apple-touch-icon', href: `/logo.jpg` }],
         // ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#be313f' }],
         // ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
         // ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
     ],
-    serviceWorker: true,
+    serviceWorker: false,
     themeConfig: {
         repo: '390405712/blog',
         editLinks: true,
-        docsDir: 'docs',
+        docsDir: 'dist',
         editLinkText: '在 GitHub 上编辑此页',
         lastUpdated: '上次更新',
         sidebar: [
